ae384b3b66a28929d1690395fd45953d49c7de49
kantundpeterpan <heiner.atze@gmx.net>
2021-11-06 23:34:22 +0100

    211106_2334

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 from distutils.core import setup
-setup(name='NCBItools', version='0.1', py_modules=['NCBItools'])
+from pip.req import parse_requirements
+import os
+
+install_reqs = parse_requirements(os.path.dirname(__file__))
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name='NCBItools', version='0.1', py_modules=['NCBItools'], install_requires = reqs)
